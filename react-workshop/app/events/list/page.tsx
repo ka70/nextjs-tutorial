badeee638e63fe249b0315c0997153383b2a8610
ka70 <ka70ka70@gmail.com>
2023-08-04 00:02:28 +0900

    second commit

---
+++ react-workshop/app/events/list/page.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import Headline from "@/components/atoms/Headline";
+import Card from "@/components/molecules/Card";
+import { useEffect, useState } from "react";
+
+export default function EventList() {
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        console.log("countが更新されました")
+    }, [count])
+
+    return (
+        <div>
+            <Headline title="testここはイベント一覧画面です" />
+            <Card
+                title="【React入門】絶対に躓かないReact（Next.js）ワークショップ #2"
+                description="最近フロントエンドを学び始めた人や、これからフロントエンドを学んで行きたい人向けに、Reactのハンズオンを開催します！"
+                startAt="2023-07-27 19:30"
+                url="https://okinawa-frontend.connpass.com/event/289268/"
+            />
+            <p>count: {count}</p>
+            <button className="rounded-lg bg-purple-500 px-8 py-3 text-center text-sm hover:bg-purple-600" onClick={() => setCount(count + 1)}>カウントアップ</button>
+        </div>
+    )
+}
---
+++ react-workshop/components/atoms/Headline.tsx
@@ -0,0 +1,5 @@
+export default function Headline(props: { title: string }) {
+    return (
+        <h1>{props.title}</h1>
+    )
+}
