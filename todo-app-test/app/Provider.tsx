a01beb26c5ab9a2881b13d6b2dccf0737e42faae
ShunKanamaru <b0271@n-koei.co.jp>
2023-08-09 12:00:49 +0900

    初期テーマの追加

--- todo-app-test/app/Provider.tsx
+++ todo-app-test/app/Provider.tsx
@@ -1,7 +1,23 @@
 "use client";
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
+import React from 'react';
 
-import { ChakraProvider } from "@chakra-ui/react";
+const config = {
+    initialColorMode: 'dark',
+    useSystemColorMode: false,
+};
+
+export const theme = extendTheme({
+    config,
+});
 
 export default function Provider({ children }: { children: React.ReactNode }) {
-    return <ChakraProvider>{children}</ChakraProvider>;
+    return (
+        <React.StrictMode>
+            <ChakraProvider theme={theme}>
+                <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+                {children}
+            </ChakraProvider>
+        </React.StrictMode>
+    );
 }
